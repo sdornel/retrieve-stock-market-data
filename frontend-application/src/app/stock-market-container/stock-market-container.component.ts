9b8ae1a089faa97a39b41d57a74c43e6e276e02b
sdornel <kai_dornel@hotmail.com>
2024-04-01 21:04:20 -0400

    moved logic down one level to avoid input/output nesting

--- frontend-application/src/app/stock-market-container/stock-market-container.component.ts
+++ frontend-application/src/app/stock-market-container/stock-market-container.component.ts
@@ -6,72 +6,55 @@ import { MatIconModule } from '@angular/material/icon';
 import { StockMarketApiService } from '../services/stock-market-api.service';
 import { HttpClientModule } from '@angular/common/http';
 import { Subject, takeUntil } from 'rxjs';
+import { StockMarketGraphComponent } from './stock-market-graph/stock-market-graph.component';
 
 @Component({
   selector: 'app-stock-market-container',
   standalone: true,
-  imports: [MatToolbarModule, MatMenuModule, MatIconModule, HttpClientModule],
+  imports: [MatToolbarModule, MatMenuModule, MatIconModule, HttpClientModule, StockMarketGraphComponent],
   providers: [StockMarketApiService],
   templateUrl: './stock-market-container.component.html',
   styleUrl: './stock-market-container.component.css'
 })
 export class StockMarketContainerComponent {
-  private $destroy = new Subject<void>();
+  // private $destroy = new Subject<void>();
   // Create a WebSocket connection
-  ws = new WebSocket('ws://localhost:3000');
+  // ws = new WebSocket('ws://localhost:3000');
 
   constructor(
     // @Inject(PLATFORM_ID) private platformId: Object,
-    private stockMarketApiService: StockMarketApiService
+    // private stockMarketApiService: StockMarketApiService
   ) {}
   
   ngOnInit(): void {
-    // see https://blogs.halodoc.io/handling-subscription-angular/ when you have child components
+    // // see https://blogs.halodoc.io/handling-subscription-angular/ when you have child components
     // this.stockMarketApiService.fetchCandlestickData().pipe(takeUntil(this.$destroy)).subscribe((data) => {
     //   console.log('Candlestick data:', data);
     // });
 
 
-    // Event handler for when the connection is established
-    this.ws.onopen = () => {
-      console.log('Connected to server');
-    };
+    // // Event handler for when the connection is established
+    // this.ws.onopen = () => {
+    //   console.log('Connected to server');
+    // };
 
-    // Event handler for receiving messages from the server
-    this.ws.onmessage = (event) => {
-      const message = JSON.parse(event.data);
-      console.log('message', message);
-    };
+    // // Event handler for receiving messages from the server
+    // this.ws.onmessage = (event) => {
+    //   const message = JSON.parse(event.data);
+    //   console.log('message', message);
+    // };
 
-    // Event handler for handling disconnection
-    this.ws.onclose = () => {
-      console.log('Disconnected from server');
-    };
-    // const socket = new WebSocket('wss://ws.finnhub.io?token=cnuu2o9r01qub9j02akgcnuu2o9r01qub9j02al0');
-
-// // Connection opened -> Subscribe
-// socket.addEventListener('open', function (event) {
-//     socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'AAPL'}))
-//     socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'}))
-//     socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'IC MARKETS:1'}))
-// });
-
-// // Listen for messages
-// socket.addEventListener('message', function (event) {
-//     console.log('Message from server ', event.data);
-// });
-
-// Unsubscribe
-//  var unsubscribe = function(symbol) {
-//     socket.send(JSON.stringify({'type':'unsubscribe','symbol': symbol}))
-// }
+    // // Event handler for handling disconnection
+    // this.ws.onclose = () => {
+    //   console.log('Disconnected from server');
+    // };
   }
 
-  ngOnDestroy(): void {
-    this.$destroy.next();
-    this.$destroy.complete();
-    if (this.ws) {
-      this.ws.close();
-    }
-  }
+  // ngOnDestroy(): void {
+  //   this.$destroy.next();
+  //   this.$destroy.complete();
+  //   if (this.ws) {
+  //     this.ws.close();
+  //   }
+  // }
 }
--- frontend-application/src/app/stock-market-container/stock-market-graph/stock-market-graph.component.ts
+++ frontend-application/src/app/stock-market-container/stock-market-graph/stock-market-graph.component.ts
@@ -1,12 +1,80 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ApexAxisChartSeries, ApexChart, ApexTitleSubtitle, ApexXAxis, ApexYAxis, NgApexchartsModule } from 'ng-apexcharts';
+import { StockMarketApiService } from '../../services/stock-market-api.service';
+import { Subject, takeUntil } from 'rxjs';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-stock-market-graph',
   standalone: true,
-  imports: [],
+  imports: [NgApexchartsModule, NgIf],
+  providers: [StockMarketApiService],
   templateUrl: './stock-market-graph.component.html',
   styleUrl: './stock-market-graph.component.css'
 })
-export class StockMarketGraphComponent {
+export class StockMarketGraphComponent implements OnInit {
+  private $destroy = new Subject<void>();
 
+  ws = new WebSocket('ws://localhost:3000');
+  candlestickData: Array<any> = [];
+  series: ApexAxisChartSeries = [{
+    name: 'candle',
+    data: []
+  }];
+  chart: ApexChart = {
+    type: 'candlestick',
+    height: 350,
+    toolbar: {
+      show: false
+    },
+  };
+  xaxis: ApexXAxis = {
+    type: 'datetime'
+  };
+  title: ApexTitleSubtitle = {
+    text: '',
+    align: 'left'
+  };
+
+  constructor(
+    private stockMarketApiService: StockMarketApiService,
+  ) {}
+
+  ngOnInit(): void {
+    // it might make sense to move all of this one level up due to options selector component
+    // see https://blogs.halodoc.io/handling-subscription-angular/ when you have child components
+    this.stockMarketApiService.fetchCandlestickData().pipe(takeUntil(this.$destroy)).subscribe((data) => {
+      this.title.text = data.meta.symbol;
+      this.series[0].data = data.values.map((item: any) => ({
+        x: item.datetime,
+        y: [item.open, item.high, item.low, item.close]
+      }));;
+      console.log('Candlestick data:', data);
+      console.log('title', this.title);
+    });
+
+    // Event handler for when the connection is established
+    this.ws.onopen = () => {
+      console.log('Connected to server');
+    };
+
+    // Event handler for receiving messages from the server
+    this.ws.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      console.log('message', message);
+    };
+
+    // Event handler for handling disconnection
+    this.ws.onclose = () => {
+      console.log('Disconnected from server');
+    };
+  }
+
+  ngOnDestroy(): void {
+    this.$destroy.next();
+    this.$destroy.complete();
+    if (this.ws) {
+      this.ws.close();
+    }
+  }
 }
