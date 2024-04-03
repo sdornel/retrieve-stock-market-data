b7582b9f71e228eaa7de0b74df0f85b801cee510
sdornel <kai_dornel@hotmail.com>
2024-04-02 21:41:03 -0400

    created a form

--- frontend-application/src/app/stock-market-container/stock-market-options-selector/stock-market-options-selector.component.ts
+++ frontend-application/src/app/stock-market-container/stock-market-options-selector/stock-market-options-selector.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-stock-market-options-selector',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './stock-market-options-selector.component.html',
   styleUrl: './stock-market-options-selector.component.css'
 })
-export class StockMarketOptionsSelectorComponent {
+export class StockMarketOptionsSelectorComponent implements OnInit {
+  optionsForm!: FormGroup;
 
   constructor() {}
 
-  // i will require a form for this component
+  ngOnInit(): void {
+    this.setupForm();
+  }
+
+  setupForm() {
+    this.optionsForm = new FormGroup({
+      symbol: new FormControl(''),
+      interval: new FormControl(''),
+      startDate: new FormControl(''),
+      endDate: new FormControl('')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.optionsForm.value);
+  }
 }
