215d056979865786947850ace3dc225095152d81
sdornel <kai_dornel@hotmail.com>
2024-04-05 18:05:45 -0400

    tests

--- frontend-application/src/app/app.component.spec.ts
+++ frontend-application/src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'frontend-for-testing-rate-limiter' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend-for-testing-rate-limiter');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- frontend-application/src/app/services/stock-market-api.service.spec.ts
+++ frontend-application/src/app/services/stock-market-api.service.spec.ts
@@ -1,16 +1,62 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { StockMarketApiService } from './stock-market-api.service';
 
 describe('StockMarketApiService', () => {
   let service: StockMarketApiService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [StockMarketApiService]
+    });
     service = TestBed.inject(StockMarketApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    // Ensure that there are no outstanding requests after each test
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('fetchCandlestickData should make GET request', () => {
+    const testData = { data: 'test data' };
+
+    service.fetchCandlestickData();
+
+    const req = httpTestingController.expectOne(req => req.url.includes('fetchCandlestickData') && req.method === 'GET');
+    
+    expect(req.request.method).toEqual('GET');
+
+    // Respond with mock data, simulating a server response
+    req.flush(testData);
+
+    // Subscribe to the result and verify the outcome
+    service.getCandlestickDataObservable().subscribe(data => {
+      expect(data).toEqual(testData);
+    });
+  });
+
+  it('should set candlestick data upon successful fetch', () => {
+    const mockData = { someKey: 'someValue' };
+    service.fetchCandlestickData();
+
+    const req = httpTestingController.expectOne({
+      method: 'GET',
+      url: 'http://localhost:3000/api/fetchCandlestickData?symbol=AAPL&interval=1day&startDate=2024-01-03&endDate=2024-01-28'
+    });
+
+    // Assert the response is flushed with mock data
+    req.flush(mockData);
+
+    // Verify the internal BehaviorSubject is updated as expected
+    service.candlestickData$.subscribe((data) => {
+      expect(data).toEqual(mockData);
+    });
   });
 });
