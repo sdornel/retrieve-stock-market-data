4ff0bf1a6d4f08faadc6eae6407ec5009a18f5f9
sdornel <kai_dornel@hotmail.com>
2024-04-05 15:49:40 -0400

    cleanup

--- frontend-application/src/app/app.component.ts
+++ frontend-application/src/app/app.component.ts
@@ -10,5 +10,5 @@ import { StockMarketContainerComponent } from './stock-market-container/stock-ma
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend-for-testing-rate-limiter';
+
 }
--- frontend-application/src/app/stock-market-container/stock-market-container.component.ts
+++ frontend-application/src/app/stock-market-container/stock-market-container.component.ts
@@ -1,11 +1,9 @@
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { StockMarketApiService } from '../services/stock-market-api.service';
 import { HttpClientModule } from '@angular/common/http';
-import { Subject, takeUntil } from 'rxjs';
 import { StockMarketGraphComponent } from './stock-market-graph/stock-market-graph.component';
 
 @Component({
@@ -17,44 +15,5 @@ import { StockMarketGraphComponent } from './stock-market-graph/stock-market-gra
   styleUrl: './stock-market-container.component.css'
 })
 export class StockMarketContainerComponent {
-  // private $destroy = new Subject<void>();
-  // Create a WebSocket connection
-  // ws = new WebSocket('ws://localhost:3000');
-
-  constructor(
-    // @Inject(PLATFORM_ID) private platformId: Object,
-    // private stockMarketApiService: StockMarketApiService
-  ) {}
-  
-  ngOnInit(): void {
-    // // see https://blogs.halodoc.io/handling-subscription-angular/ when you have child components
-    // this.stockMarketApiService.fetchCandlestickData().pipe(takeUntil(this.$destroy)).subscribe((data) => {
-    //   console.log('Candlestick data:', data);
-    // });
-
-
-    // // Event handler for when the connection is established
-    // this.ws.onopen = () => {
-    //   console.log('Connected to server');
-    // };
-
-    // // Event handler for receiving messages from the server
-    // this.ws.onmessage = (event) => {
-    //   const message = JSON.parse(event.data);
-    //   console.log('message', message);
-    // };
-
-    // // Event handler for handling disconnection
-    // this.ws.onclose = () => {
-    //   console.log('Disconnected from server');
-    // };
-  }
-
-  // ngOnDestroy(): void {
-  //   this.$destroy.next();
-  //   this.$destroy.complete();
-  //   if (this.ws) {
-  //     this.ws.close();
-  //   }
-  // }
+  constructor() {}
 }
