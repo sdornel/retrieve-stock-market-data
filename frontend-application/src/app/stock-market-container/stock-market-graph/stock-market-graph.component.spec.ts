9b6bec0916c08ef0ebbb8f1c315f0da035a87bb9
sdornel <kai_dornel@hotmail.com>
2024-04-06 15:11:49 -0400

    tests

--- frontend-application/src/app/stock-market-container/stock-market-graph/stock-market-graph.component.spec.ts
+++ frontend-application/src/app/stock-market-container/stock-market-graph/stock-market-graph.component.spec.ts
@@ -1,36 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { StockMarketGraphComponent } from './stock-market-graph.component';
 import { StockMarketApiService } from '../../services/stock-market-api.service';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BehaviorSubject, of } from 'rxjs';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CandlestickData } from '../../types/types';
 
-fdescribe('StockMarketGraphComponent', () => {
+describe('StockMarketGraphComponent', () => {
   let component: StockMarketGraphComponent;
   let fixture: ComponentFixture<StockMarketGraphComponent>;
-  let stockMarketApiServiceMock: any;
+  let mockStockMarketApiService: any;
   let websocketSpy: jasmine.SpyObj<WebSocket>;
 
   beforeEach(async () => {
-    stockMarketApiServiceMock = {
-      fetchCandlestickData: jasmine.createSpy('fetchCandlestickData'),
-      candlestickData$: null, // new BehaviorSubject(null)
-    };
-
     websocketSpy = jasmine.createSpyObj('WebSocket', ['send', 'close', 'onopen', 'onmessage', 'onclose']);
     spyOn(window, 'WebSocket').and.returnValue(websocketSpy as unknown as WebSocket);
 
     await TestBed.configureTestingModule({
       imports: [StockMarketGraphComponent, ReactiveFormsModule, HttpClientTestingModule],
-      // declarations: [StockMarketGraphComponent],
-      providers: [
-        { provide: StockMarketApiService, useValue: stockMarketApiServiceMock }
-      ]
+      providers: [StockMarketApiService]
     }).compileComponents();
+
 
     fixture = TestBed.createComponent(StockMarketGraphComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
@@ -55,18 +47,74 @@ fdescribe('StockMarketGraphComponent', () => {
     });
   });
 
-  // describe('fetchCandlestickData', () => {
-  //   beforeEach(() => {
-  //     stockMarketApiServiceMock.candlestickData$.and.returnValue(of({
-  //       meta: 'hi',
-  //       status: 'ok',
-  //       values: [{ hi: 'hi' }],
-  //     }))
-  //     component.fetchCandlestickData();
-  //   });
+  describe('fetchCandlestickData()', () => {
+    describe('data is returned', () => {
+      const mockData: CandlestickData = {
+        meta: {
+          symbol: 'TEST',
+          exchange: 'Test Exchange',
+          exchange_timezone: 'Test/TimeZone',
+          interval: '1d',
+          type: 'asdf',
+          mic_code: 'miccode',
+          currency: 'USD',
+        },
+        status: 'ok',
+        values: [
+          { datetime: '2024-04-01', open: '1', high: '2', low: '0.5', hightime: 'asdf', close: '1', volume: '3' },
+          { datetime: '2024-04-02', open: '1.5', high: '2.5', low: '1', hightime: 'asdf', close: '2', volume: '4' }
+        ],
+      };
+      beforeEach(fakeAsync(() => {
+        mockStockMarketApiService = fixture.debugElement.injector.get(StockMarketApiService);
+        mockStockMarketApiService.candlestickData$.next(mockData);
+        component.fetchCandlestickData();
+      }));
+  
+      it('retrieves data and assigns state', () => {
+        expect(component.title).toEqual({ text: mockData.meta.symbol, align: 'left' });
+        expect(component.exchange).toEqual(mockData.meta.exchange);
+        expect(component.exchangeTimezone).toEqual(mockData.meta.exchange_timezone);
+        expect(component.interval).toEqual(mockData.meta.interval);
+        expect(component.startDate).toEqual(mockData.values[0].datetime);
+        expect(component.endDate).toEqual(mockData.values[mockData.values.length-1].datetime);
+        expect(component.series[0].data[0]).toEqual({
+          x: mockData.values[0].datetime,
+          y: [
+            mockData.values[0].open,
+            mockData.values[0].high,
+            mockData.values[0].low,
+            mockData.values[0].close,
+          ],
+        });
+      });
+    });
 
-  //   it('', () => {
-  //     expect(1).toEqual(2);
-  //   });
-  // });
+    describe('no data is returned', () => {
+      const mockData: CandlestickData = {
+        meta: {
+          symbol: '',
+          exchange: '',
+          exchange_timezone: '',
+          interval: '',
+          type: '',
+          mic_code: '',
+          currency: '',
+        },
+        status: 'error',
+        values: [],
+      };
+      beforeEach(fakeAsync(() => {
+        mockStockMarketApiService = fixture.debugElement.injector.get(StockMarketApiService);
+        mockStockMarketApiService.candlestickData$.next(mockData);
+        component.fetchCandlestickData();
+      }));
+  
+      it('retrieves data and assigns state', () => {
+        // these are the only two that really matter if nothing gets returned
+        expect(component.title).toEqual({ text: 'AAPL', align: 'left' });
+        expect(component.series.length).toEqual(1);
+      });
+    });
+  });
 });
