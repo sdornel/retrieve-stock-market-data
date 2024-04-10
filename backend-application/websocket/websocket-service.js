d6d0530f3e479c45ffd4420b7cb3e0fbc278714e
sdornel <kai_dornel@hotmail.com>
2024-04-10 16:13:59 -0400

    cleaning up

--- backend-application/websocket/websocket-service.js
+++ backend-application/websocket/websocket-service.js
@@ -73,52 +73,6 @@ class WebsocketService {
             }
         });
     }
-
-    // initializeWebsocket = (symbol) => {
-    //     this.finnhubWs.on('open', () => {
-    //         console.log('Connected to Finnhub WebSocket');
-    //         this.finnhubWs.send(JSON.stringify({ 'type': 'subscribe', 'symbol': symbol }));
-    //         // finnhubWs.send(JSON.stringify({ 'type': 'subscribe', 'symbol': 'BINANCE:BTCUSDT' }));
-    //         // finnhubWs.send(JSON.stringify({ 'type': 'subscribe', 'symbol': 'IC MARKETS:1' }));
-    //     });
-        
-    //     function broadcastData(data) {
-    //         wss.clients.forEach(client => {
-    //             if (client.readyState === WebSocket.OPEN) {
-    //                 client.send(data); // Forward Finnhub data
-    //             }
-    //         });
-    //     }
-        
-    //     this.finnhubWs.on('message', (data) => {
-    //         console.log('Data from Finnhub:', data.toString());
-    //         broadcastData(data.toString()); // Only pass the data after converting it to a string
-    //     });
-        
-    //     // Handling errors for Finnhub WebSocket connection
-    //     this.finnhubWs.on('error', (error) => {
-    //         console.error('Error in Finnhub WebSocket connection:', error);
-    //     });
-        
-    //     // When your WebSocket server gets a new connection
-    //     wss.on('connection', connection = (ws) => {
-    //         console.log('A client connected');
-    //         ws.send(JSON.stringify({ message: 'Welcome! Connection established.' }));
-    //         ws.on('error', (error) => {
-    //             console.error('Error in client WebSocket connection:', error);
-    //         });
-    //         // onOpenFinnhubSocket(wss);
-    //         ws.on('close', () => {
-    //             console.log('Client disconnected');
-    //             unsubscribe('AAPL');
-    //         });
-    //     });
-        
-    //     // Unsubscribe
-    //     var unsubscribe = (symbol) => {
-    //         this.finnhubWs.send(JSON.stringify({'type':'unsubscribe','symbol': symbol}))
-    //     }
-    // }
 }
 
 module.exports = { WebsocketService };
